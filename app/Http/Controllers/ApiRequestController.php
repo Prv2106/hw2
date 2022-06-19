ac02c84ac7f06b3cacf20fd354403dfd9e7c6f41
Alberto Provenzano <101663958+Prv2106@users.noreply.github.com>
2022-06-19 13:05:59 +0200

    Add files via upload

---
+++ app/Http/Controllers/ApiRequestController.php
@@ -0,0 +1,346 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Routing\Controller;
+use App\Models\User;
+use App\Models\Chat;
+use App\Models\Favorite;
+use App\Models\MovieList;
+
+
+
+class ApiRequestController extends Controller {
+
+
+    function popularMovies($page){
+        $session = session('username');
+        $user = User::find($session);
+        if (!isset($session)){ 
+            return redirect('login');
+        }
+
+
+        $endpoint = 'https://api.themoviedb.org/3/movie/popular?api_key=' . env('TMDB_API_KEY') .  '&language=it&region=IT&page=';
+    
+
+        $url = $endpoint . $page;
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL,$url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($curl);
+
+        $json = json_decode($result, true);
+        
+        curl_close($curl);
+
+        $newJson = array();
+
+        for ($i = 0; $i < count($json['results']); $i++) {
+            $favorites=false;
+            $movie_id = $json['results'][$i]['id'];
+            $exists = $user->favorites()->where('movie_id',$movie_id)->first();
+            if($exists != null){
+                $favorites=true;
+            }
+            
+            $shared = false;
+            $shared_query = $user->chat()->where('movie_id',$movie_id)->first();
+            if($shared_query != null){
+                $shared=true;
+            }
+            
+            $list = false;
+            $m_id = strval($movie_id);
+            $list_query = MovieList::where("username",$session)->where('movie_id',$m_id)->first();
+            if($list_query != null){
+                $list = true;
+            }
+            
+            $newJson[] = array(
+                'id' => $json['results'][$i]['id'],
+                'title' => $json['results'][$i]['title'],
+                'poster_path' => $json['results'][$i]['poster_path'],
+                'overview'=>$json['results'][$i]['overview'],
+                'vote_average' => $json['results'][$i]['vote_average'],
+                'release_date' => $json['results'][$i]['release_date'],
+                'popularity' => $json['results'][$i]['popularity'],
+                'favorites' => $favorites,
+                'list' => $list,
+                'shared' => $shared
+            );
+        }
+
+        return $newJson;
+
+    }
+
+
+    function searchMovies(){
+
+        $session = session('username');
+        $user = User::find($session);
+        if (!isset($session)){ 
+            return redirect('login');
+        }
+        $movie = request('movie');
+
+        $endpoint = 'https://api.themoviedb.org/3/search/movie?api_key=' . env('TMDB_API_KEY') . '&language=it&region=IT&query=';
+
+    
+        
+        
+        
+        $movie = urlencode($movie);
+        $url = $endpoint . $movie;
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL,$url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($curl);
+
+        
+        $json = json_decode($result, true);
+
+        curl_close($curl);
+
+        $newJson = array();
+
+
+        for ($i = 0; $i < count($json['results']); $i++) {
+            
+            $favorites=false;
+            $movie_id = $json['results'][$i]['id'];
+            $exists = $user->favorites()->where('movie_id',$movie_id)->first();
+            if($exists != null){
+                $favorites=true;
+            }
+            
+            $shared = false;
+            $shared_query = $user->chat()->where('movie_id',$movie_id)->first();
+            if($shared_query != null){
+                $shared=true;
+            }
+
+
+            $list = false;
+            $m_id = strval($movie_id);
+            $list_query = MovieList::where("username",$session)->where('movie_id',$m_id)->first();
+            if($list_query != null){
+                $list = true;
+            }
+            
+            
+
+            $newJson[] = array(
+                'res' => $json['results'][$i],
+                'favorites' => $favorites,
+                'list' => $list,
+                'shared' => $shared
+            );
+
+    
+        }
+
+        return $newJson;
+
+
+    }
+
+
+
+    function topRatedMovies($page){
+        $session = session('username');
+        $user = User::find($session);
+        if (!isset($session)){ 
+            return redirect('login');
+        }
+
+
+        $endpoint = 'https://api.themoviedb.org/3/movie/top_rated?api_key=' . env('TMDB_API_KEY') . '&language=it&region=IT&page=';
+
+        
+
+        $url = $endpoint . $page;
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL,$url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($curl);
+
+        $json = json_decode($result, true);
+
+        curl_close($curl);
+
+        $newJson = array();
+
+        for ($i = 0; $i < count($json['results']); $i++) {
+            $favorites=false;
+            $movie_id = $json['results'][$i]['id'];
+            $exists = $user->favorites()->where('movie_id',$movie_id)->first();
+            if($exists != null){
+                $favorites=true;
+            }
+            
+            $shared = false;
+            $shared_query = $user->chat()->where('movie_id',$movie_id)->first();
+            if($shared_query != null){
+                $shared=true;
+            }
+
+            $list = false;
+            $m_id = strval($movie_id);
+            $list_query = MovieList::where("username",$session)->where('movie_id',$m_id)->first();
+            if($list_query != null){
+                $list = true;
+            }
+            
+            $newJson[] = array(
+                'id' => $json['results'][$i]['id'],
+                'title' => $json['results'][$i]['title'],
+                'poster_path' => $json['results'][$i]['poster_path'],
+                'overview'=>$json['results'][$i]['overview'],
+                'vote_average' => $json['results'][$i]['vote_average'],
+                'release_date' => $json['results'][$i]['release_date'],
+                'popularity' => $json['results'][$i]['popularity'],
+                'favorites' => $favorites,
+                'list' => $list,
+                'shared' => $shared
+            );
+        }
+
+        return $newJson;
+
+    }
+
+
+
+
+    function getGenreList(){
+        $session = session('username');
+        $user = User::find($session);
+        if (!isset($session)){ 
+            return redirect('login');
+        }
+
+
+        $url = 'https://api.themoviedb.org/3/genre/movie/list?api_key=' . env('TMDB_API_KEY') .  '&language=it&region=IT';
+    
+        
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL,$url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($curl);
+
+        
+        $json = json_decode($result, true);
+
+        curl_close($curl);
+
+        $newJson = array();
+
+        for ($i = 0; $i < count($json['genres']); $i++) {
+            $newJson[] = array(
+                'name' => $json['genres'][$i]['name'],
+                'id' => $json['genres'][$i]['id']
+            );
+        }
+
+        return $newJson;
+    }
+
+
+
+    function searchByGenre($id,$page){
+
+        $session = session('username');
+        $user = User::find($session);
+        if (!isset($session)){ 
+            return redirect('login');
+        }
+
+        $endpoint = 'https://api.themoviedb.org/3/discover/movie?api_key=' . env('TMDB_API_KEY') . '&language=it&region=IT&with_genres=';
+
+
+        $url = $endpoint . $id . "&page=" . $page;
+
+
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL,$url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($curl);
+
+        
+        $json = json_decode($result, true);
+
+        curl_close($curl);
+
+        $newJson = array();
+
+        for ($i = 0; $i < count($json['results']); $i++) {
+            $movie_id = $json['results'][$i]['id'];
+            $favorites=false;
+            $exists = $user->favorites()->where('movie_id',$movie_id)->first();
+            if($exists != null){
+                $favorites=true;
+            }
+            
+            $shared = false;
+            $shared_query = $user->chat()->where('movie_id',$movie_id)->first();
+            if($shared_query != null){
+                $shared=true;
+            }
+            
+            $list = false;
+            $m_id = strval($movie_id);
+            $list_query = MovieList::where("username",$session)->where('movie_id',$m_id)->first();
+            if($list_query != null){
+                $list = true;
+            }
+            
+            $newJson[] = array(
+                'id' => $json['results'][$i]['id'],
+                'title' => $json['results'][$i]['title'],
+                'poster_path' => $json['results'][$i]['poster_path'],
+                'overview'=>$json['results'][$i]['overview'],
+                'vote_average' => $json['results'][$i]['vote_average'],
+                'release_date' => $json['results'][$i]['release_date'],
+                'popularity' => $json['results'][$i]['popularity'],
+                'favorites' => $favorites,
+                'list' => $list,
+                'shared' => $shared
+            );
+        }
+
+        return $newJson;
+    }
+
+
+    function youtubeTrailer($search){
+
+        $session = session('username');
+        $user = User::find($session);
+        if (!isset($session)){ 
+            return redirect('login');
+        }
+
+        $endpoint = 'https://www.googleapis.com/youtube/v3/search?part=snippet&relevanceLanguage=it&videoEmbeddable=true&type=video&maxResults=1&key='
+            . env('YOUTUBE_API_KEY') . '&q=';
+
+
+        $url = $endpoint . urlencode($search);    
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL,$url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($curl);
+
+        
+        $json = json_decode($result, true);
+
+        return $json;           
+
+
+    }
+
+}
+?>
---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,168 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Routing\Controller;
+use App\Models\User;
+use App\Models\Favorite;
+use App\Models\Chat;
+use Illuminate\Http\Request;
+
+class ChatController extends Controller {
+
+    public function index() {
+        $session = session('username');
+        $user = User::find($session);
+        if (!isset($user))
+            return redirect('login');
+        
+        return view("chat")->with("user", $user);
+    }
+
+
+
+    function addMsg(){
+        $request = request();
+        $session = session('username');
+        $user = User::find($session);
+        if (!isset($user))
+            return redirect('login');
+
+        $query =new Chat;
+        $query->username = $session;
+        $query->movie_id = $request -> movie_id;
+        $query->img = $request->img;
+        $query->text_msg = $request->text;
+        $query->title = $request->title;
+        $query->updated = 0;
+        $query->save();
+        if($query){
+            $response = array("op" => true); 
+        }
+        else{
+            $response = array("op" => false); 
+        }
+    
+        return $response;
+
+    
+
+    }
+
+
+
+    function showMsg($type=null,$id=null){
+        $session = session('username');
+        $user = User::find($session);
+        if (!isset($user))
+            return redirect('login');
+
+        $status=false;
+
+        if(($type==null)||($id==null)){
+            $results = Chat::orderByDesc('msg_id')->limit(10)->get();
+            $n_res = Chat::orderByDesc('msg_id')->count();
+            if($n_res!=0){ 
+                $status=true;
+            }   
+            
+        }
+
+        if(($type=="first")&& ($id!=null)){
+            $results = Chat::where('msg_id',"<=",$id)->orderByDesc('msg_id')->limit(10)->get();
+            $n_res = Chat::orderByDesc('msg_id')->count();
+            if($n_res!=0){ 
+                $status=true;
+            } 
+        }
+
+        if(($type=="last")&& ($id!=null)){
+            $results = Chat::where('msg_id',"<",$id)->orderByDesc('msg_id')->limit(10)->get();
+            $n_res = Chat::where('msg_id',"<",$id)->orderByDesc('msg_id')->count();
+            if($n_res!=0){ 
+                $status=true;
+            }   
+        }
+
+        if(($type=="current")&& ($id!=null)){
+            $results = Chat::where('msg_id',"<=",$id)->orderByDesc('msg_id')->limit(10)->get();
+            $n_res = Chat::where('msg_id',"<=",$id)->orderByDesc('msg_id')->count();
+            if($n_res!=0){ 
+                $status=true;
+            }
+        }
+
+        $json = array("n_res" => $n_res,"results" => $results,"status" => $status);
+        return $json;
+    }
+
+
+
+    function getUsername(){
+        $session = session('username');
+        $user = User::find($session);
+        if (!isset($user))
+            return redirect('login');
+
+        return session('username');
+    }
+
+
+
+
+    function removeMsg($id){
+        $session = session('username');
+        $user = User::find($session);
+        if (!isset($user)){ 
+            return redirect('login');
+        }
+        
+        $exists = $user->chat()->where('msg_id',$id)->first();
+        if($exists == null){
+            $response = array("op" => false);
+            return $response;
+        }
+        $exists->delete();
+        $response = array("op" => true);
+        return $response;
+    }
+
+
+
+
+    function updateMsg(){
+
+        
+        $session = session('username');
+        $user = User::find($session);
+        if (!isset($user))
+            return redirect('login');
+            
+        $request = request();
+
+
+
+        $update = $user->chat()->where('msg_id',$request->msg_id)->first();
+        
+
+        $update->text_msg = $request->text;
+        $update->updated = 1;
+        
+        $update->save();
+
+        if ($update) {
+            $response = array("op" => true); 
+        } 
+        else {
+            $response = array("op" => false); 
+        }
+
+        return $response;
+
+
+
+    }
+
+
+
+
+}
+?>
