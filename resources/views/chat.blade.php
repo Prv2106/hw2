ae0be69dc6abbae731017bcd93008b005376c11b
Alberto Provenzano <101663958+Prv2106@users.noreply.github.com>
2022-06-22 16:39:11 +0200

    Add files via upload

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.page')
+
+
+@section('title', ' Chat')
+
+
+@section('css')
+<link rel="stylesheet" href='{{ asset('css/chat.css') }}'/> 
+@endsection
+
+
+@section('script')
+<script src='{{ asset('js/chat.js') }}' defer></script>
+@endsection
+
+
+@section('home_link')
+<a href="{{ route('home') }}"><h1>Top Movies</h1></a>
+@endsection
+
+@section('links')
+<a href="{{ route('home') }}">Home</a>
+<a href="{{ route('genre') }}">Genere</a>
+<a href="{{ route('top_rated') }}">Piu' votati</a>
+<a href="{{ route('favorites') }}">Preferiti</a>
+<a href="{{ route('watch_list') }}">Watch List</a>
+<a href= "{{ route('logout') }}">Logout</a>
+@endsection
+
+
+@section('header')
+<header>
+    <article>
+        <h1>Cerca film</h1> 
+        <span>Ciao {{ $user['name'] }} consiglia agli altri utenti di Top Movies qualche film da guardare!</span>
+        <form class ="search">
+            <input type='text' id='search' value = "nome del film">
+            <input type='submit' class='submit' value='Cerca'>
+            <div id="close" data-content="close" class="hidden"><img  src ='{{ asset('images/x-regular-24.png') }}'/></div>
+        </form>
+        <img  class="circle hidden"  src ='{{ asset('images/circle-loading-gif.gif') }}'/>
+        <article data-section = "movies">
+            <section id = "album-view"></section>
+        </article>
+    <article>          
+</header>
+@endsection
+
+
+
+@section('contents')
+<article id="content">            
+    <article id ="main-view">
+        <section id="chat-display" class="empty">
+            <img  class="circle" src ='{{ asset('images/circle-loading-gif.gif') }}'/>
+        </section>
+    </article>             
+</article>
+@endsection
